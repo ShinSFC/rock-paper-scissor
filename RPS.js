02939c201f957fb2d41f6532b421305c50f97e67
ShinpeiH <dctechsf@gmail.com>
2024-06-18 22:13:54 -0700

    remove computerChoice and humanChoice variables

--- RPS.js
+++ RPS.js
@@ -6,17 +6,11 @@ const computerChoiceArray = [
 
 function getComputerChoice() {
     return computerChoiceArray[Math.floor(Math.random() * computerChoiceArray.length)]
-}
-
-let computerChoice = getComputerChoice()
-// console.log(computerChoice) this works
+} //this works
 
 function getHumanChoice() {
     return prompt("Type rock, paper or scissor:")
-}
-
-let humanChoice = getHumanChoice() //this invokes getHumanChoice()
-// console.log(humanChoice)
+} // this works
 
 var humanScore = 0
 var computerScore = 0
@@ -25,4 +19,6 @@ function playRound(humanChoice, computerChoice) {
     getHumanChoice()
     console.log(humanChoice)
 }
+
+
 
