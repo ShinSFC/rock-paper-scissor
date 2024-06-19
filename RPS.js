c2640466af7fc9f00f891891ba84f2abe762b5ff
ShinpeiH <dctechsf@gmail.com>
2024-06-18 23:57:24 -0700

    finished function playRound

--- RPS.js
+++ RPS.js
@@ -16,14 +16,30 @@ var humanScore = 0
 var computerScore = 0
 
 function playRound(humanChoice, computerChoice) {
-    // switch (humanChoice) {
-   
+    humanChoice = humanChoice.toLowerCase() // this works
+    console.log(humanChoice)
+    console.log(computerChoice)
+    if (humanChoice == computerChoice) {
+        console.log("Tie!") // this works
+    } else if ((humanChoice == "rock") && (computerChoice == "paper")) {
+        console.log("You lose!")
+    } else if ((humanChoice == "rock") && (computerChoice == "scissors")) {
+        console.log("You win!")
+    } else if ((humanChoice == "paper") && (computerChoice == "scissors")) {
+        console.log("You Lose!")
+    } else if ((humanChoice == "paper") && (computerChoice == "rock")) {
+        console.log("You win!") // works till this point
+    } else if ((humanChoice == "scissors") && (computerChoice == "paper")) {
+        console.log("You lose!")
+    } else if ((humanChoice == "scissors") && (computerChoice == "rock")) {
+        console.log("You win!")
+    }
 }
 
-const humanSelection = getHumanChoice()
-const computerSelection = getComputerChoice()
+const humanSelection = getHumanChoice() //this works
+const computerSelection = getComputerChoice() //this works
 
-playRound(getHumanChoice, getComputerChoice)
+playRound(humanSelection, computerSelection)
 
 
 
